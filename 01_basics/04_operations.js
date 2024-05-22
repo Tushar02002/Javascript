4b01ff207976dbaed99cd87bf335b502993d4fd7
Utkarsh174 <utkarshmall174@gmail.com>
2024-05-22 15:01:58 +0000

    multiple operators

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,27 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Utkarsh"
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(1 + "2" + 2 + "2")
+// console.log(1 + 2 + "2" + 2)
+
+// console.log(+true)    => 1
+// console.log(+false)   => 0
+// console.log(+" ")     => 0
+// console.log(+"")      => 0
+// console.log(+"abc")   => NaN
+
+let gameCounter = 100
+// let a = gameCounter++ => a = 100
+// let b = ++gameCounter => b = 101
+console.log(b)
