4353a84cfe9afdab8aff43b7c927f30b33e00f4f
Utkarsh174 <utkarshmall174@gmail.com>
2024-05-25 07:42:57 +0000

    basics of strings

---
+++ 01_basics/08_strings.js
@@ -0,0 +1,24 @@
+const name = "Utkarsh"
+
+const repoCount = 50
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Utkarsh abcd')
+
+// console.log(gameName.charAt(0))        // U
+
+// console.log(gameName.slice(0, 3))      // Utk
+
+const newStingOne = "   abcd   "
+// console.log(newStingOne.trim())        // abcd
+
+const url = "https://utkarsh.com/utkarsh%20abcd"
+
+// console.log(url.replace('%20', '-'))   // https://utkarsh.com/utkarsh-abcd
+ 
+// console.log(url.includes('utkarsh'))   // true
+
+// console.log(gameName.split(" "))       // [ 'Utkarsh', 'abcd' ]
+
+
