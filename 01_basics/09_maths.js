d4c82d04160663b1ab252d514665170b19bc1bb3
Utkarsh174 <utkarshmall174@gmail.com>
2024-05-26 04:47:18 +0000

    maths and numbers

---
+++ 01_basics/09_maths.js
@@ -0,0 +1,17 @@
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))     // 10,00,000
+
+// console.log(Math)                                 // Object [Math] {}
+
+// console.log(Math.abs(-4))                         // 4
+// console.log(Math.round(4.3))                      // 4
+// console.log(Math.ceil(4.2))                       // 5
+// console.log(Math.floor(4.3))                      // 4
+// console.log(Math.min(4, 3, 6, 8))                 // 3
+// console.log(Math.random())                        // Between 0 and 1
+// console.log((Math.random() * 10) + 1)             // to avoid the value of 0.01 
+                                                     // add 1
+const min = 10
+const max = 20
+
+// console.log(Math.random() * (max - min + 1))
