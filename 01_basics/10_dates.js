32cda7c4e412bfb4e9f4ff2387d9a5c9264c2149
Utkarsh174 <utkarshmall174@gmail.com>
2024-05-26 05:33:54 +0000

    date and time

---
+++ 01_basics/10_dates.js
@@ -0,0 +1,11 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString())     // Sun May 26 2024 04:55:39 GMT+0000 
+                                      // (Coordinated Universal Time)
+
+// console.log(typeof myDate)         // object
+
+newDate.toLoacaleString('default', {
+    weekday: "long",
+})
